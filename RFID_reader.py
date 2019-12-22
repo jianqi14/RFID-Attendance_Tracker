59165a991639ad50f9278efcad5360a71989445a
Jian Qi <jianxin1997@yahoo.com>
2019-12-21 22:24:20 -0800

    slight improvement on reader functionality

--- RFID_reader.py
+++ RFID_reader.py
@@ -14,18 +14,18 @@ try:
 		id, text = reader.read()
 
 		print("Item Read, these are the information on the Card/Tag:")
-		print("Card ID Number: ", id)
-		print("Card Message: ", text)
+		print("\n\tCard ID Number: ", id)
+		print("\tCard Message: ", text)
 
 
 		print("\nWould you like to read another Card/Tag?")
 		readAgain = input("Enter Yes/No: ")
 
-		if readAgain == "No" or readAgain == "no":
+		if readAgain == "No" or readAgain == "no" or readAgain == 'n' or readAgain == 'N':
 			repeatRead = False
 			print("\n RFID Reader Ended. \n Thank You, See you Later.")
 
-		elif readAgain == "Yes" or readAgain == "yes":
+		elif readAgain == "Yes" or readAgain == "yes" or readAgain == 'Y' or readAgain == 'y':
 			repeatRead = True
 			print("\n")
 
